a8991d293489616bea514ff3b2a91deb15630d82
Shivam-Khandare <shivamsk3019@gmail.com>
2024-10-13 22:29:55 +0530

    first commit

---
+++ src/Message.tsx
@@ -0,0 +1,6 @@
+function Message(){
+    const name = 'Shivam';
+    return <h1>Hello {name}</h1>
+}
+
+export default Message;
---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,28 @@
+function ListGroup() {
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+
+  const getMessage = () => {
+    return items.length === 0 ? <p>No item found</p> : null;
+  };
+
+  return (
+    <>
+      <h1>List</h1>
+      {items.length === 0 ? <p>No item found</p> : null}
+      {items.length === 0 && <p>No item found</p>}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            className="list-group-item"
+            key={item}
+            onClick={() => console.log(item, index)}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
